f3f6a8059c91e1776d5b87b19733b247f78c8f51
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-03-07 16:32:24 +0100

    Initialize project

---
+++ app.config.ts
@@ -0,0 +1,51 @@
+import { type ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: 'filmlogger',
+  slug: 'filmlogger',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'myapp',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+  },
+  web: {
+    bundler: 'metro',
+    output: 'static',
+    favicon: './assets/images/favicon.png',
+  },
+  plugins: [
+    'expo-router',
+    [
+      'expo-splash-screen',
+      {
+        image: './assets/images/splash-icon.png',
+        imageWidth: 200,
+        resizeMode: 'contain',
+        backgroundColor: '#ffffff',
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    router: {
+      origin: false,
+    },
+    eas: {
+      projectId: '257b5deb-8936-45c3-ba09-74669a634359',
+    },
+  },
+};
+
+export default config;
