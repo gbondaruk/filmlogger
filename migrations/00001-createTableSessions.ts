882fc210a929536a476d1e8f563565ee5520b292
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-04-07 16:12:11 +0200

    update dependencies

--- migrations/00001-createTableSessions.ts
+++ migrations/00001-createTableSessions.ts
@@ -1,22 +1,7 @@
-import type { Sql } from 'postgres';
+export async function up(client) {
 
-export type Session = {
-  id: number;
-  token: string;
-  userId: number;
-};
-
-export async function up(sql: Sql) {
-  await sql`
-    CREATE TABLE sessions (
-      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-      token varchar(150) NOT NULL UNIQUE,
-      expiry_timestamp timestamp NOT NULL DEFAULT now() + interval '24 hours',
-      user_id integer NOT NULL REFERENCES users (id) ON DELETE CASCADE
-    );
-  `;
 }
 
-export async function down(sql: Sql) {
-  await sql`DROP TABLE sessions`;
+export async function down(client) {
+
 }
--- migrations/00002-createTableCamerasFL.ts
+++ migrations/00002-createTableCamerasFL.ts
@@ -1,28 +1,7 @@
-import type { Sql } from 'postgres';
-import { z } from 'zod';
+export async function up(client) {
 
-export type Camerafl = {
-  id: number;
-  brand: string;
-  model: string;
-  userId: number;
-};
-
-export const cameraflSchema = z.object({
-  brand: z.string().min(1).max(30),
-  model: z.string().min(1).max(30),
-});
-
-export async function up(sql: Sql) {
-  await sql`
-    CREATE TABLE camerasfl (
-      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-      brand varchar(30) NOT NULL,
-      model varchar(30) NOT NULL,
-      user_id integer NOT NULL REFERENCES users (id)
-    )
-  `;
 }
-export async function down(sql: Sql) {
-  await sql`DROP TABLE camerasfl`;
+
+export async function down(client) {
+
 }
